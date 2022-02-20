2cbd2007a7b1cd02be006f69a32a6d6c167a6f33
Alberto Madrazo <betomadrazo@gmail.com>
2022-02-19 19:43:35 -0600

    Lecture 170: Re-Integrating Attributes. Add class Attributes and a constructor to the User class.

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,8 +1,10 @@
 export class Attributes<T> {
   constructor(private data: T) { }
-
-  get(propName: string): number | string {
-    return this.data[propName];
+  // <K extends keyof T> Sets up a generic constraint
+  // it can only be one of the keys of T
+  // T[K] is just like a normal look up
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
   }
 
   set(update: T): void {
